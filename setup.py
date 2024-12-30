680f1ba535a8c437bc4f8cdc1976333200ac0c0b
sabahatatta <sabahatatta@gmail.com>
2024-12-30 10:51:10 +0500

    Initial Commit - Folder Structure Created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sabahat Atta',
+    author_email= 'sabahatatta@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
