3d3a1c9be8d649677284318670579ffb31452139
Sabahat Atta <50008506+sabahatatta@users.noreply.github.com>
2025-01-09 01:16:37 +0500

    Update setup.py
    Removed email information

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'Generative AI Project',
     version= '0.0.0',
     author= 'Sabahat Atta',
-    author_email= 'sabahatatta@gmail.com',
+    author_email= 'xxxxx@gmail.com',
     packages= find_packages(),
     install_requires = []
 
